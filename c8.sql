666340226d7519ff53e29b8a5152ba50360af265
Hoang Cao Quoc Thang <thang.hoang7020202@hcmut.edu.vn>
2022-10-12 15:56:05 +1000

    add c8 has not fix

---
+++ c8.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW `CustomerRating` AS
+SELECT S.customer, M.rating, COUNT(M.rating) AS content 
+FROM Streams S
+JOIN Movie M ON (M.prefix = S.moviePrefix AND M.suffix = S.movieSuffix)
+GROUP BY M.rating,S.customer
+ORDER BY S.customer;
+
+CREATE OR REPLACE VIEW `MaxRating` AS 
+SELECT CR.customer, MAX(CR.content) AS RR
+FROM CustomerRating CR
+GROUP BY CR.customer
+HAVING MAX(CR.content);
+
+SELECT CR.customer, CR.rating
+FROM CustomerRating CR
+JOIN MaxRating MR ON (CR.customer = MR.customer)
+WHERE CR.content = MR.RR;
