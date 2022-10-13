b19c37a0e6c6ecc54ecf7b3e8c3bb8a89e0122b5
vietnguyen <nghoangviet.english@gmail.com>
2022-10-13 13:33:18 +1000

    Fix c1.sql sort by y,m,d des

--- c1.sql
+++ c1.sql
@@ -1,4 +1,4 @@
-SELECT C.id, C.name 
-FROM Customer C 
-WHERE C.subscriptionLevel = "Basic"
-ORDER BY TIMESTAMPDIFF(YEAR , C.dob, NOW()) ASC;
+SELECT C.id, C.name, C.dob
+FROM Customer C
+WHERE C.subscriptionLevel LIKE 'basic'
+ORDER BY YEAR(C.dob) DESC, MONTH(C.dob) DESC, DAY(C.dob) DESC;
