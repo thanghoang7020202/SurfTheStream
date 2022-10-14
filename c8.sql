ac0411c3e13903a76f8e9a2a796926836de36fc6
Hoang Cao Quoc Thang <thang.hoang7020202@hcmut.edu.vn>
2022-10-14 15:19:08 +1000

    reduce number of query in c8 (4 -> 3)

--- c8.sql
+++ c8.sql
@@ -1,21 +1,20 @@
-CREATE VIEW `CustomerRating` AS
+CREATE OR REPLACE VIEW `CustomerRating` AS
 SELECT S.customer, M.rating, COUNT(M.rating) AS content 
 FROM Streams S
 JOIN Movie M ON (M.prefix = S.moviePrefix AND M.suffix = S.movieSuffix)
 GROUP BY M.rating,S.customer
 ORDER BY S.customer;
 
-CREATE VIEW `MaxRating` AS 
-SELECT CR.customer, MAX(CR.content) AS RR
-FROM CustomerRating CR
-GROUP BY CR.customer
-HAVING MAX(CR.content);
-
-CREATE VIEW `result` AS
+CREATE OR REPLACE VIEW `result` AS
 SELECT CR.customer, CR.rating
 FROM CustomerRating CR
-JOIN MaxRating MR ON (CR.customer = MR.customer)
-WHERE CR.content = MR.RR;
+WHERE CR.content IN (
+    SELECT MAX(CR2.content)
+    FROM CustomerRating CR2
+    WHERE CR2.customer = CR.customer
+    GROUP BY CR2.customer
+    HAVING MAX(CR.content)
+	);
 
 SELECT R1.customer, R1.rating
 FROM result R1
